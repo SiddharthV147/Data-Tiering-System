4e06b9c1537487a2bd7df671667cd62d927d4c36
SiddharthV147 <siddharthvarade147@gmail.com>
2026-01-09 22:32:00 +0530

    Initialized Application

---
+++ src/main/java/com/app/dts/controller/Hello.java
@@ -0,0 +1,13 @@
+package com.app.dts.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+
+  @GetMapping("/")
+  public String helloFunc() {
+    return "Hello Function Called";
+  }
+}
---
+++ src/test/java/com/app/dts/DtsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.dts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DtsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
